e57317d4e664663bcc8b55d930f9358ba47bfc09
naila <nailabelkhiri23@gmail.com>
2026-05-17 23:36:39 +0200

    fix mysql keep alive

--- smarthire_backend/config/db.js
+++ smarthire_backend/config/db.js
@@ -1,13 +1,20 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  port: Number(process.env.DB_PORT) || 3306,
+
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
+
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+
   ssl: {
     rejectUnauthorized: false,
   },
@@ -17,7 +24,7 @@ const db = pool.promise();
 
 db.getConnection()
   .then((connection) => {
-    console.log("✅ MySQL Railway Connected");
+    console.log("✅ MySQL Connected");
     connection.release();
   })
   .catch((err) => {
